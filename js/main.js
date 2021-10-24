de25b2230cb3ccd57b85d0125101304f5af9b20a
takemoto222 <omochi9797@icloud.com>
2021-10-24 14:47:41 +0900

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-odcument.getElementById(id)
+
+
+
+//ハンバーガー
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+target.classList.toggle('open');
+const nav = document.getElementById("nav");
+nav.classList.toggle('in');
+});
+
+
+//横スクロール
+function countScroll(){
+  const target = document.getElementById('target');
+  const x = target.scrollLeft;
+  document.getElementById('output').innerHTML = x;
+}
+
+
+//pcの上下スクロール
+function scrollTop(elem) {
+  const target = document.getElementById(elem);
+  target.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
+
+scrollTop('button');
